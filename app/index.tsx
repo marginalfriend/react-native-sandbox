373ff4e9f0cfcaa19fda965968b6550cddae6637
marginalfriend <abdullahpsg@gmail.com>
2024-07-15 16:20:41 +0700

    [NEW] fonts and color palettes

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { StatusBar, Text, View } from "react-native";
 export default function App() {
   return (
 		<View className="flex-1 items-center justify-center bg-white">
-			<Text className="text-3xl">Notes of Arkad</Text>
+			<Text className="text-3xl text-bla">Notes of Arkad</Text>
 			<StatusBar barStyle={"default"} />
 			<Link href={"/profile"} style={{color: 'blue'}}>Go to Profile</Link>
 		</View>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	theme: {
+		extend: {
+			colors: {
+				background: "#E9ECEF",
+				midground: "#CED4DA",
+				foreground: "#6C757D",
+				black: "#212529",
+				highlight: "#1985a1",
+				danger: "#a4161a"
+			},
+			fontFamily: {
+				thin: {
+					DEFAULT: ["JetbrainsJetBrainsMono-Thin", "sans-serif"],
+					italic: ["JetBrainsMono-ThinItalic", "sans-serif"],
+				},
+				light: {
+					DEFAULT: ["JetbrainsJetBrainsMono-Light", "sans-serif"],
+					italic: ["JetBrainsMono-LightItalic", "sans-serif"],
+				},
+				regular: {
+					DEFAULT: ["JetbrainsJetBrainsMono-Regular", "sans-serif"],
+					italic: ["JetBrainsMono-RegularItalic", "sans-serif"],
+				},
+				medium: {
+					DEFAULT: ["JetbrainsJetBrainsMono-Medium", "sans-serif"],
+					italic: ["JetBrainsMono-MediumItalic", "sans-serif"],
+				},
+				semibold: {
+					DEFAULT: ["JetbrainsJetBrainsMono-SemiBold", "sans-serif"],
+					italic: ["JetBrainsMono-SemiBoldItalic", "sans-serif"],
+				},
+				bold: {
+					DEFAULT: ["JetbrainsJetBrainsMono-Bold", "sans-serif"],
+					italic: ["JetBrainsMono-BoldItalic", "sans-serif"],
+				},
+			},
+		},
+	},
+	plugins: [],
+};
