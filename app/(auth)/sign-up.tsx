6c9a31357aeccf8134eda095ccc32bff1004d919
marginalfriend <abdullahpsg@gmail.com>
2024-07-16 10:29:53 +0700

    [NEW] sign up screen

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,12 +1,76 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { SafeAreaView, View, Text, ScrollView, Image } from "react-native";
+import React, { useState } from "react";
+import Images from "@/constants/Images";
+import FormField from "@/components/FormField";
+import ThemedButton from "@/components/ThemedButton";
+import { Link } from "expo-router";
 
 const SignUp = () => {
-	return (
-		<View>
-			<Text>SignUp</Text>
-		</View>
-	)
-}
+  const [form, setForm] = useState({
+		username: "",
+    email: "",
+    password: "",
+  });
 
-export default SignUp
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleSubmit = () => {};
+
+  return (
+    <SafeAreaView className="h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full min-h-[85vh] px-4 my-6">
+          <Image
+            source={Images.logo}
+            className="w-[200px] h-[100px] mx-auto"
+            resizeMode="contain"
+          />
+          <Text className="text-center text-2xl font-xbold">
+            Sign Up to Arkad
+          </Text>
+          <FormField
+            placeHolder="Username"
+            title="Username"
+            value={form.username}
+            handleChange={(e: any) => {
+              setForm({ ...form, username: e });
+            }}
+            formStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            placeHolder="Email"
+            title="Email"
+            value={form.email}
+            handleChange={(e: any) => {
+              setForm({ ...form, email: e });
+            }}
+            formStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            placeHolder="Password"
+            title="Password"
+            value={form.password}
+            handleChange={(e: any) => {
+              setForm({ ...form, password: e });
+            }}
+            formStyles="mt-7"
+            keyboardType="default"
+          />
+
+          <ThemedButton text="Sign In" handlePress={handleSubmit} />
+
+          <View className="justify-center pt-5 flex flex-row gap-2">
+            <Text className="font-regular">Already have an account?</Text>
+            <Link href="/sign-in" className="font-jbold text-highlight">
+              Sign In
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignUp;
